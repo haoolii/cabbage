5b91011213aa6ec9667c45225418a923070b75c0
unnhao <unnhao@gmail.com>
2025-02-28 14:16:47 +0800

    feat: add base auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 // import { getRecordDetail } from "./request/requests";
 
 // function checkIsUniqueID(uniqueId: string) {
 //   return /^[a-zA-Z0-9]+$/.test(uniqueId);
 // }
 
-export async function middleware(/*request: NextRequest*/) {
-  const response = NextResponse.next();
+// export async function middleware(/*request: NextRequest*/) {
+//   const response = NextResponse.next();
 
   // const uniqueId = request.nextUrl.pathname.slice(1);
 
@@ -16,10 +16,31 @@ export async function middleware(/*request: NextRequest*/) {
   //     response.cookies.set("Authorization", `Bearer ${token}`);
   // }
 
-  return response;
+//   return response;
+// }
+
+export function middleware(request: NextRequest) {
+  const baseAuth = request.headers.get("authorization");
+
+  const url = request.nextUrl;
+
+  if (baseAuth) {
+    const authValue = baseAuth.split(" ")[1];
+    const [user, pwd] = atob(authValue).split(":");
+
+    if (user === "admin" && pwd === "yyds") {
+      return NextResponse.next();
+    }
+  }
+  url.pathname = "/api/auth";
+
+  return NextResponse.rewrite(url);
 }
 
 // 設定匹配的路徑
+// export const config = {
+//   matcher: ["/:uniqueId([a-zA-Z0-9]+)"], // 只允許英文字母和數字
+// };
 export const config = {
-  matcher: ["/:uniqueId([a-zA-Z0-9]+)"], // 只允許英文字母和數字
+  matcher: ["/:path*"],
 };
